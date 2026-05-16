b57c416e7e222c1ce5fa24ed805847a6636c8ae2
Sanni Yusuf Olamilakan <ysanni560@gmail.com>
2026-05-16 12:38:45 +0100

    Added claims extraction file

---
+++ src/extractor.py
@@ -0,0 +1,103 @@
+import re
+import os
+import sys
+# Add the parent directory to sys.path to allow imports from the prompts package
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+import spacy
+from groq import Groq
+from dotenv import load_dotenv
+
+from prompts.prompts import grammar_fix_prompt, seller_claim_extraction_prompt
+
+
+nlp = spacy.load("en_core_web_sm")
+
+def load_api_key():
+    load_dotenv()
+    api_key = os.getenv("GROQ_API_KEY_1")
+    return api_key
+
+
+def preprocess_description(text: str) -> str:
+  """Cleans product descriptions by standardizing whitespace, fixing formatting, and removing duplicate sentences."""
+
+  # normalise white space
+  text = re.sub(r'\s+', ' ', text)
+
+  # Inserts a period when a lowercase letter is immediately followed by an uppercase letter
+  text = re.sub(r'([a-z])([A-Z])', r'\1. \2', text)
+
+  # Replaces the Unicode non-breaking space with a normal space
+  text = text.replace('\xa0', ' ')
+
+  seen = set()
+  unique = []
+
+  doc = nlp(text)
+  for sent in doc.sents:
+      normalized = sent.text.strip().lower()
+      if not normalized:
+          continue
+      if normalized not in seen:
+          seen.add(normalized)
+          unique.append(sent.text.strip())
+
+  return " ".join(unique)
+
+
+def fix_description_grammar(description: str) -> str:
+    """
+    Fix grammar, punctuation, and run-on sentences in a product description
+    without changing any claims.
+    """
+
+    try:
+        api_key = load_api_key()
+        client = Groq(api_key=api_key)
+
+        response = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",  # or your preferred model
+            messages=[{"role": "user", "content": grammar_fix_prompt.format(description=description)}]
+        )
+
+        fixed_text = response.choices[0].message.content
+        return fixed_text
+
+    except Exception as e:
+        print("Error fixing grammar:", e)
+        return None
+
+
+def extract_sellers_claim_from_description(description: str):
+  """ Extracts sellers claims from the cleaned description and returns them in json format """
+  try:
+    api_key = load_api_key()
+    client2 = Groq(api_key=api_key)
+
+    chat_completion = client2.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": seller_claim_extraction_prompt.format(description=description),
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
+    claims_jsons = chat_completion.choices[0].message.content
+    return claims_jsons
+
+  except Exception as e:
+    print(f"Error extracting claims {e}")
+
+
+def extract_claims(description):
+   preprocessed_description = preprocess_description(text=description)
+   cleaned_description = fix_description_grammar(description=preprocessed_description)
+   seller_claims = extract_sellers_claim_from_description(description=cleaned_description)
+
+   print(seller_claims)
+   return seller_claims
+
+   
+   
