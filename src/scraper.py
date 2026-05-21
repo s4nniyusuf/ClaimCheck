3186f5d7178ec7ed78f70d9f8322367d2a5ec414
Sanni Yusuf Olamilakan <ysanni560@gmail.com>
2026-05-21 22:54:14 +0100

    Replace UserAgent.random() with a static user agent string in browser setup

--- src/scraper.py
+++ src/scraper.py
@@ -21,10 +21,9 @@ def set_up_browser(playwright):
         )
     context = browser.new_context(
         user_agent=(
-            # "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            # "AppleWebKit/537.36 (KHTML, like Gecko) "
-            # "Chrome/120.0.0.0 Safari/537.36"
-            UserAgent.random()
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/120.0.0.0 Safari/537.36"
         )
     )
     page = context.new_page()
